df939e4da6dccdba5d4b766df2ae3ec76e835cf7
patipat <patipat@omise.co>
2019-02-26 21:48:54 +0700

    inital project

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+var bot *linebot.Client
+
+func main() {
+	var err error
+	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	http.HandleFunc("/linemadi", linemadi)
+	http.ListenAndServe("0.0.0.0:6969", nil)
+}
+
+func linemadi(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+		}
+	}
+}
